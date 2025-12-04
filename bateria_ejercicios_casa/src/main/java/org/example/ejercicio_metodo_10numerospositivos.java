0a09e3452192f2d8ebfb7762c55bbfeea8594079
AlexPerse10 <spuknitfire@gmail.com>
2025-12-04 14:27:59 +0100

    ejercicios realizados de metodos

--- bateria_ejercicios_casa/src/main/java/org/example/ejercicio_metodo_10numerospositivos.java
+++ bateria_ejercicios_casa/src/main/java/org/example/ejercicio_metodo_10numerospositivos.java
@@ -1,5 +1,24 @@
 package org.example;
 
-public class ejercicio_metodo_10numerospositivos {
+import java.util.Scanner;
 
+public class ejercicio_metodo_10numerospositivos {
+    public static int[] dieznumeros() {
+        Scanner sc = new Scanner(System.in);
+        int numero[] = new int[10];
+        int contador = 0;
+        for (int i = 0; i < numero.length; i++) {
+            System.out.println("introduce el numero de la posicion " + (i + 1));
+            int numero_usuario = sc.nextInt();
+            if (numero_usuario >= 0) {
+                numero[contador] = numero_usuario;
+                contador++;
+            }
+        }
+        int[] positivos = new int[contador];
+        for (int i = 0; i < contador; i++) {
+            positivos[i] = numero[i];
+        }
+        return positivos;
+    }
 }
